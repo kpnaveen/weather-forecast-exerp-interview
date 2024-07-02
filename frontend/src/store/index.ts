2177e457464185ac223eb70cfbc1be743fd8119a
kpnaveen <naveenkp@scg.com>
2024-07-02 20:36:25 +0530

    Added selected place details in the store and its required actions, getters and mutation. Also included logger for developement mode only

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,14 +1,35 @@
-import { createStore } from 'vuex'
+import { createStore, createLogger } from 'vuex';
+
+const plugins: any[] = [];
+
+if(process.env.NODE_ENV === "development") {
+  plugins.push(createLogger())
+}
 
 export default createStore({
   state: {
+    selectedPlace: {
+      lat: null,
+      lng: null,
+      test: process.env
+    }
   },
   getters: {
+    selectedPlace: (state) => {
+      return state.selectedPlace
+    }
   },
   mutations: {
+    setSelectedPlace: (state, payload) => {
+      state.selectedPlace = payload
+    }
   },
   actions: {
+    updateSelectedPlace: (context, payload) => {
+      context.commit("setSelectedPlace", payload)
+    }
   },
   modules: {
-  }
+  },
+  plugins: plugins
 })
